1f6986f504f0b2030256a68fdadaab55fb0e7a7f
jaesunkim <jskim.faz@gmail.com>
2017-09-04 11:49:32 +0900

    formatting

--- launcher/logchain_launcher.py
+++ launcher/logchain_launcher.py
@@ -4,8 +4,8 @@ from peerproperty import set_peer
 from storage import file_controller
 from communication import receiver
 
-def main():
 
+def main():
     'Remove all transaction in mempool'
     file_controller.remove_all_transactions()
 
@@ -13,13 +13,13 @@ def main():
     my_ip_address = file_controller.get_my_ip()
     property.my_ip_address = my_ip_address
     set_peer.set_peer()
-    print ("my peer : " + str(property.my_peer_num))
+    print("my peer : " + str(property.my_peer_num))
 
     'reveiver thread start'
-    threading._start_new_thread(receiver.start("Receiver", my_ip_address, property.port))
-    print ("RECEIVER START")
+    threading._start_new_thread(receiver.start(
+        "Receiver", my_ip_address, property.port))
+    print("RECEIVER START")
 
 
 if __name__ == '__main__':
     main()
-
