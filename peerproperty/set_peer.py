a342b46ab67de76694ab815ad46724e68e9595ee
jaesunkim <jskim.faz@gmail.com>
2017-09-20 10:03:43 +0900

    불필요한 get my ip 제거

--- peerproperty/set_peer.py
+++ peerproperty/set_peer.py
@@ -3,14 +3,15 @@ from storage import file_controller
 
 
 def set_peer():
-    my_ip = file_controller.get_my_ip()
-    if my_ip == nodeproperty.Peer1:
+
+    # my_ip = file_controller.get_my_ip()
+    if nodeproperty.my_ip_address == nodeproperty.Peer1:
         nodeproperty.my_peer_num = 1
-    elif my_ip == nodeproperty.Peer2:
+    elif nodeproperty.my_ip_address == nodeproperty.Peer2:
         nodeproperty.my_peer_num = 2
-    elif my_ip == nodeproperty.Peer3:
+    elif nodeproperty.my_ip_address == nodeproperty.Peer3:
         nodeproperty.my_peer_num = 3
-    elif my_ip == nodeproperty.Peer4:
+    elif nodeproperty.my_ip_address == nodeproperty.Peer4:
         nodeproperty.my_peer_num = 4
     else:
         nodeproperty.my_peer_num = "API_Peer"
