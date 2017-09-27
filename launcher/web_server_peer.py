8a2a85380e28d90ecbc9705ff4b127173318a04c
jaesunkim <jskim.faz@gmail.com>
2017-09-27 14:34:43 +0900

    오타 수정

--- launcher/web_server_peer.py
+++ launcher/web_server_peer.py
@@ -9,7 +9,6 @@ from communication.p2p import receiver
 from communication.p2p import sender
 from communication.p2p import node_mapping_table
 
-from restapi_dispatch import restapi_call_dispatcher
 
 # restapi_node_launcher로 교체될 예정....
 
