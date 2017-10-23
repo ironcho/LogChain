742c473f2f1f5d99e60bb8731b47e948f3655d6e
ChoSooHwan <Soohwancho@outlook.com>
2017-10-24 02:13:47 +0900

    commit

--- launcher/logchain_launcher.py
+++ launcher/logchain_launcher.py
@@ -20,8 +20,8 @@ def main():
     nodeproperty.my_ip_address = file_controller.get_my_ip()
     set_peer.set_peer()
 
-    print("my peer num : " + str(nodeproperty.my_peer_num))
-    print(" ")
+    print("my peer num  : " + str(nodeproperty.my_peer_num))
+  
 
     # node_mapping_table.set_node()와 set_peer()는 중복 기능이나, 일단 디버깅용으로 중복으로 유지함
     node_mapping_table.set_node()
