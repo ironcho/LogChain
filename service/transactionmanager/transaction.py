ad9e81d1f9ccd7ea13133b1ad4b489cb6cf58561
jaesunkim <jskim.faz@gmail.com>
2017-10-25 16:15:01 +0900

    transaction class param 수정

--- service/transactionmanager/transaction.py
+++ service/transactionmanager/transaction.py
@@ -8,7 +8,7 @@ class Transaction(object):
     """
 
     # def __init__(self, p_recv_addr, p_extra):
-    def __init__(self, p_extra,transaction_count):  # rule tx에서는 recv addr이 불요하므로 제거
+    def __init__(self, p_extra):  # rule tx에서는 recv addr이 불요하므로 제거
         """
 
         :param p_recv_addr:
@@ -18,7 +18,7 @@ class Transaction(object):
         self.timestamp = time.strftime('%Y%m%d%H%M%S', time.localtime())
         self.tx_id = "B" + self.timestamp
         self.extra_data = self.extra_data = p_extra
-        self.transaction_count = transaction_count
+        # self.transaction_count = transaction_count
         # self.message = ''
         # self.pub_key = ''
         # self.signature = ''
