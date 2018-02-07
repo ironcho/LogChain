0830f727181b6272f677c1c008dceeaf70a7c8d6
Jae Sun Kim <jskim.faz@gmail.com>
2018-02-07 11:57:30 +0900

    p2p refactoring

--- restapi_dispatch/save_tx_queue.py
+++ restapi_dispatch/save_tx_queue.py
@@ -6,7 +6,7 @@ from restapi_dispatch import queue_strategy
 import json
 from service.transactionmanager import transaction
 from communication.p2p import sender
-
+from peerproperty import nodeproperty
 
 class SaveTxQueueThread(threading.Thread):
     def __init__(self, p_thrd_id, p_thrd_name, p_inq):
@@ -17,6 +17,14 @@ class SaveTxQueueThread(threading.Thread):
 
     def run(self):
         receive_event(self.thrd_name, self.inq)
+
+    def save_sensorinfo(self, p_sensorinfo_json):
+        logging.debug('request(save sensor info) rcvd...')
+        self.inq.put(p_sensorinfo_json)
+        logging.debug(str(self.inq))
+        logging.debug(self.inq.qsize())
+
+
 
 
 def receive_event(p_thrd_name, p_inq):
@@ -30,7 +38,8 @@ def receive_event(p_thrd_name, p_inq):
         temp = json.dumps(
             tx, indent=4, default=lambda o: o.__dict__, sort_keys=True)
 
-        sender.send_to_all(temp)  # 노드들 연동 후 테스트 필요 2017-09-27
+        # sender.send_to_all(temp)
+        sender.send_to_all_peers(temp,nodeproperty.My_receiver_port)
 
         logging.debug(str(dequeued))
         logging.debug(str(temp))
