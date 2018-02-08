759533c3c7ce4025a9648e13dc4d2736cedbb921
Jae Sun Kim <jskim.faz@gmail.com>
2018-02-08 09:46:08 +0900

    rest api node for testing

--- service/transactionmanager/transaction_generator.py
+++ service/transactionmanager/transaction_generator.py
@@ -1,7 +1,12 @@
 import logging
 
+
 def save_sensorinfo(p_sensorinfo_json):
     logging.debug('request(save sensor info) rcvd...')
     savetx_q.put(p_sensorinfo_json)
     logging.debug(str(savetx_q))
     logging.debug(savetx_q.qsize())
+
+
+
+
