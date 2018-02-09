5dd68d1d8a8cade5faad9fd1c7818f58d0a97061
Jae Sun Kim <jskim.faz@gmail.com>
2018-02-09 09:07:27 +0900

    debug-voting

--- service/blockconsensus/voting.py
+++ service/blockconsensus/voting.py
@@ -9,7 +9,7 @@ from communication.peermgr import peermgr
 from monitoring import monitoring
 
 TransactionCountForConsensus = 5
-MajorityVoting = nodeproperty.Total_peer_num * 0.5
+MajorityVoting = 1
 
 def blind_voting(merkle_root):
 
